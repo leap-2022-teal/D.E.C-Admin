1a2875ea3972ee6d5ad3d8d2ab3747ee93a7fd0c
Evening <evening@admins-iMac-1-8.local>
2023-04-12 15:48:11 +0800

    hi

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div style={{ color: "green" }}>Hello NextJS ADMIN DEC Team project</div>
+      <div style={{ color: "green" }}>
+        Hello NextJS ADMIN DEC Team project is hi
+      </div>
     </>
   );
 }
