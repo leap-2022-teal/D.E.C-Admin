cd5cb7e6c803d0a4bf318d6cc49b21dfd77061b6
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-04-16 15:13:28 +0800

    admin front end product and categories first style

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,5 +1,9 @@
 import MainLayout from "@/components/MainLayout";
+import ProductAddModal from "@/components/modals/productAddModal";
+import ProductEditModal from "@/components/modals/productEditModal";
+import Search from "@/components/search";
 import React, { useEffect, useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
@@ -12,7 +16,61 @@ export default function Products() {
 
   return (
     <MainLayout>
-      <div>DEC project products</div>
+      <div>
+        <div className=" flex justify-between border-solid pb-4 border-b-2">
+          <h2 className=" font-bold ">Бараа бүтээгдэхүүн</h2>
+          <Search />
+          <ProductAddModal />
+        </div>
+        <div className="overflow-hidden rounded-lg border border-gray-50 shadow-md m-5 w-[95%]">
+          <table className="w-[100%] table-auto">
+            <thead className="bg-gray-50 h-[50px]">
+              <tr>
+                <th className=" w-40 text-gray-900" scope="col"></th>
+                <th
+                  className=" text-left font-medium text-gray-900"
+                  scope="col"
+                >
+                  Бүтээгдэхүүн
+                </th>
+                <th className="text-left font-medium text-gray-900" scope="col">
+                  Үнэ
+                </th>
+                <th
+                  className=" w-40 text-left font-medium text-gray-900"
+                  scope="col"
+                >
+                  Нөөц
+                </th>
+                <th
+                  className="text-left font-medium text-gray-900"
+                  scope="col"
+                ></th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td>
+                  <img
+                    src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
+                    alt=""
+                    className=" w-32 py-3 rounded-[5px] object-cover object-center"
+                  />
+                </td>
+                <td className=" text-left">Malcolm Lockyer</td>
+                <td className="text-left">1961</td>
+                <td className="text-left">1961</td>
+                <td className="text-left">
+                  <div className=" pr-20 flex justify-end gap-4">
+                    <ProductEditModal />
+                    <DeleteIcon className=" text-red-600 hover:text-red-300" />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </MainLayout>
   );
 }
--- pages/users.tsx
+++ pages/users.tsx
@@ -2,5 +2,5 @@ import MainLayout from "@/components/MainLayout";
 import React from "react";
 
 export default function Users() {
-  return <MainLayout>DEC project users</MainLayout>;
+  return <MainLayout>users</MainLayout>;
 }
