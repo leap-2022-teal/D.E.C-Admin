cb09dab484376a40639375e168029a9a48e18637
Evening <evening@admins-iMac-1-8.local>
2023-04-18 17:22:24 +0800

    product image'

--- components/modals/productAddModal.tsx
+++ components/modals/productAddModal.tsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-
+import CategorySelector from "../categorySelector";
+import axios from "axios";
 export default function ProductAddModal() {
   const [showModal, setShowModal] = React.useState(false);
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [price, setPrice] = useState("");
+  const [color, setColor] = useState("");
+  const [size, setSize] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+  const [uploading, setUploading] = useState(false);
+  const [image, setImage] = useState();
+  async function handleFileUpload(event: any) {
+    setUploading(true);
+    const imageFile = event.target.files[0];
 
+    const formData = new FormData();
+    formData.append("image", imageFile);
+
+    await fetch(`${process.env.REACT_APP_API_URL}/upload-image`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data);
+        setUploading(false);
+      });
+  }
+  function createProducts() {
+    axios
+      .post(`http://localhost:8000/products`, {
+        name: name,
+        details: details,
+        price: price,
+        color: color,
+        sizes: size,
+      })
+      .then((res) => {
+        const { status } = res;
+        if (status === 200) {
+          setShowModal(false);
+          setName("");
+          setDetails("");
+          setPrice("");
+          setColor("");
+          setSize("");
+        }
+      });
+  }
   return (
     <>
       <button
         onClick={() => setShowModal(true)}
-        className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded"
-      >
+        className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded">
         <AddCircleOutlineIcon className=" mr-2" />
         Бүтээгдэхүүн нэмэх
       </button>
@@ -25,8 +70,7 @@ export default function ProductAddModal() {
                   <h3 className="text-3xl font-semibold">Бүтээгдэхүүн нэмэх</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
+                    onClick={() => setShowModal(false)}>
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
@@ -35,16 +79,89 @@ export default function ProductAddModal() {
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <div className="mb-6">
+                    <CategorySelector
+                      value={categoryId}
+                      onChange={(val) => setCategoryId(val)}
+                    />
                     <label
                       htmlFor="default-input"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    ></label>
+                      className=" mt-4 block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Барааны нэр
+                    </label>
                     <input
-                      placeholder="Бүтээгдэхүүнээ оруулана уу?"
+                      placeholder=""
                       type="text"
                       id="default-input"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="   bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
+                    <label
+                      htmlFor="default-input"
+                      className=" mt-4 block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Details
+                    </label>
+                    <input
+                      placeholder=""
+                      type="text"
+                      id="default-input"
+                      value={details}
+                      onChange={(e) => setDetails(e.target.value)}
+                      className="   bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                    <label
+                      htmlFor="default-input"
+                      className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Барааны үнэ
+                    </label>
+                    <input
+                      placeholder=""
+                      type="text"
+                      id="default-input"
+                      value={price}
+                      onChange={(e) => setPrice(e.target.value)}
+                      className="   bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+
+                    <label
+                      htmlFor="default-input"
+                      className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Color
+                    </label>
+                    <input
+                      placeholder=""
+                      type="text"
+                      id="default-input"
+                      value={color}
+                      onChange={(e) => setColor(e.target.value)}
+                      className="   bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                    <label
+                      htmlFor="default-input"
+                      className="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Size
+                    </label>
+                    <input
+                      placeholder=""
+                      type="text"
+                      id="default-input"
+                      value={size}
+                      onChange={(e) => setSize(e.target.value)}
+                      className="   bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                    <div>
+                      <input
+                        type="file"
+                        name="image"
+                        onChange={handleFileUpload}
+                      />
+
+                      {uploading && (
+                        <div className="spinner-border" role="status"></div>
+                      )}
+
+                      {image && <img src={image.path} width="100" alt="" />}
+                    </div>
                   </div>
                 </div>
                 {/*footer*/}
@@ -52,15 +169,13 @@ export default function ProductAddModal() {
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
-                  >
+                    onClick={() => setShowModal(false)}>
                     хаах
                   </button>
                   <button
                     className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded block  focus:ring-4 focus:outline-none focus:ring-blue-300  text-sm  text-center "
                     type="button"
-                    onClick={() => setShowModal(false)}
-                  >
+                    onClick={createProducts}>
                     хадгалах
                   </button>
                 </div>
