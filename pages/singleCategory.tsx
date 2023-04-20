74917e00f3102a116fb481f41539215031000ce3
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-04-20 13:29:13 +0800

    subCategories delete

--- pages/singleCategory.tsx
+++ pages/singleCategory.tsx
@@ -4,17 +4,16 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import ClearIcon from "@mui/icons-material/Clear";
 
-export function SingleCategory({
-  category,
-}: // isUpdated,
-// setIsUpdated,
-any) {
-  const router = useRouter();
-
+export function SingleCategory({ category }: any) {
+  console.log(category);
   function handleDelete() {
-    if (window.confirm("Delete?")) {
+    if (window.confirm("Aнгилал устгах уу ?")) {
       axios
-        .delete(`http://localhost:8000/categories/${category._id}`)
+        .delete(
+          `http://localhost:8000/categories/${
+            category._id || category.subCategories.title._id
+          } `
+        )
         .then((res) => {
           const { status } = res;
           if (status === 200) {
@@ -29,17 +28,17 @@ any) {
     <>
       <div
         key={category._id}
-        className=" hover:bg-gray-100 flex justify-between p-2 my-2 w-[100%]"
+        className=" hover:bg-gray-100 flex justify-between p-10 my-2 w-[100%] "
       >
-        <div className=" flex items-center ">{category.name}</div>
+        <div className=" flex items-center font-bold ">{category.name}</div>
 
         <div className="">
           {category.subCategories?.map((subTitle: any) => (
             <div className=" bg-gray-100 rounded-[5px] border-solid border-1 hover:bg-gray-200 mb-2 p-2 flex justify-between">
               {subTitle.title}
-              <div>
-                <button>
-                  <ClearIcon className=" text-red-500 hover:text-red-400" />
+              <div className=" hover:bg-gray-300 rounded-[5px] ml-3">
+                <button onClick={handleDelete}>
+                  <ClearIcon className=" text-red-500 hover:text-red-400 " />
                 </button>
               </div>
             </div>
@@ -49,8 +48,11 @@ any) {
         <div className=" flex items-center">
           <CategoryEditModal category={category} />
 
-          <button onClick={handleDelete}>
-            <DeleteIcon className="text-red-500 ml-4 hover:text-red-400" />
+          <button
+            onClick={handleDelete}
+            className=" hover:bg-gray-200 rounded-[5px] w-9 h-9 "
+          >
+            <DeleteIcon className="text-red-500 " />
           </button>
         </div>
       </div>
