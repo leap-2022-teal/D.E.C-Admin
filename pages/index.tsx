a58ba5bc289acca5f652c256dda3d2f6c183b6a2
balmadselena <balmadselena@gmail.com>
2023-05-01 18:26:51 +0800

    admin: login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import MainLayout from "@/components/MainLayout";
+import Signin from "@/components/signin";
 
 export default function Home() {
-  return <MainLayout>Welcome</MainLayout>;
+  // return 
+  return(
+    <Signin/>
+  )
 }
